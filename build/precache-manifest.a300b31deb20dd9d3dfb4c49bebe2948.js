629094f1bcccd6f4ceb3dffaee03160a7b6593a5
Siegfried Alimi <siegfried_alimi@yahoo.fr>
2019-05-09 15:58:52 +0200

    Add build directory to be able to deploy from github to heroku as a static site

---
+++ build/precache-manifest.a300b31deb20dd9d3dfb4c49bebe2948.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6a533efe748a88a12e5b",
+    "url": "/static/js/main.a226fccd.chunk.js"
+  },
+  {
+    "revision": "a8a6d59df9fcbdfdcf9a",
+    "url": "/static/js/2.38fe61bb.chunk.js"
+  },
+  {
+    "revision": "6a533efe748a88a12e5b",
+    "url": "/static/css/main.176d015f.chunk.css"
+  },
+  {
+    "revision": "a8a6d59df9fcbdfdcf9a",
+    "url": "/static/css/2.fc655db1.chunk.css"
+  },
+  {
+    "revision": "7acf3a678689e4b2b1a43a9fa6d96016",
+    "url": "/index.html"
+  }
+];
